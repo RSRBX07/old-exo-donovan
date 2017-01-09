9cd43a4b0d8243e7b365abcfd345b7a81a6ab381
Dodo <donovan.persent@gmail.com>
2017-01-09 14:29:29 +0100

    Modif exo

--- Exos_Base/hello.rb
+++ Exos_Base/hello.rb
@@ -1,8 +1,15 @@
 puts "Entrez votre prénom"
-fName = gets.to_s
+fName = gets.chomp.capitalize
 puts "Entrez votre nom"
-lName = gets.to_s
+lName = gets.chomp.upcase
+
 puts "Bonjour #{fName} #{lName}!"
+
 for i in 0..2
     puts "Salut"
 end
+# ou
+3.times do puts "Salut" end
+
+initiales = fName[0] + lName[0]
+puts "Vos initiales sont #{initiales}"
---
+++ Exos_Base/numbers.rb
@@ -0,0 +1,6 @@
+# Exercice sur les nombres
+
+#1. calculer le nombre de secondes dans une année
+
+nb_sec = 60*60*24*365
+puts "Il y a #{nb_sec} dans une année"
