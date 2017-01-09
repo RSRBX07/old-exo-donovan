fc346d549ef74a55917ab17c2890c9228e5a98fa
Dodo <donovan.persent@gmail.com>
2017-01-09 15:08:19 +0100

    Update

--- Exos_Base/numbers.rb
+++ Exos_Base/numbers.rb
@@ -1,6 +1,17 @@
 # Exercice sur les nombres
 
 #1. calculer le nombre de secondes dans une année
-
 nb_sec = 60*60*24*365
 puts "Il y a #{nb_sec} dans une année"
+
+puts "Quelle est la date de votre naissance? :"
+b_year = gets.to_i
+b_month = gets.to_i
+b_day = gets.to_i
+
+birthdate = Time.new("#{b_day}/#{b_month}/#{b_year}")
+times = Time.now
+
+age_sec = times - birthdate
+
+puts "Vous êtes né il y a #{age_sec} secondes"
