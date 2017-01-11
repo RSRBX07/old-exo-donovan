d46a3ee79406c190544edc33733c42c3b3affa76
Dodo <donovan.persent@gmail.com>
2017-01-11 11:07:51 +0100

    Modif Loto

--- Exos_Base/loto.rb
+++ Exos_Base/loto.rb
@@ -3,6 +3,7 @@ cagnote = 0
 benefice = 0
 prix_billet = 2
 rand_nb_joueurs = true
+rand_tirage_joueur = true
 nb_joueurs = 1
 
 stop = false
@@ -15,17 +16,31 @@ while stop == false
     puts "#{nb_joueurs} ont participé au tirage"
 
     #calcul cagnote (20% de commission)
-    benefice += prix_billet*nb_joueurs*0.2
-    cagnote += prix_billet*nb_joueurs*0.8
+    benefice += (prix_billet*nb_joueurs*0.2).to_i
+    cagnote += (prix_billet*nb_joueurs*0.8).to_i
 
     #affichage de la cagnotte
     puts "Le montant de la cagnote est de #{cagnote} €"
+    puts "Le société a gagné #{benefice}€ au total avec les paris"
 
     #calcul du tirage
     puts "Les résultats du tirage sont : #{tirage = Array.new((1..45).to_a).shuffle.take(5).sort}"
 
     #calcul de vos numéros
-    puts "Vos numéros sont : #{votre_grille = Array.new((1..45).to_a).shuffle.take(5).sort}"
+    if rand_tirage_joueur 
+        puts "Vos numéros sont : #{votre_grille = Array.new((1..45).to_a).shuffle.take(5).sort}" #  => A éviter!
+    else
+        votre_grille = Array.new
+        puts "Veuillez saisir 5 numéros de 0 à 45 :"
+        (0..4).each do
+            input = gets.to_i
+            while input < 0 || input > 45
+                puts "Saisie erronnée, veuillez resaisir :"
+                input = gets.to_i
+            end
+            votre_grille.push(input)
+        end
+    end
 
     #Nombre de numéros trouvés
     numeros_gagnants = 0
@@ -40,8 +55,8 @@ while stop == false
 
     #Calcul montant gagné
     if numeros_gagnants != 5 && numeros_gagnants != 0
-        coef_gain = Math.sqrt(5 / numeros_gagnants)
-        montant_gagne = (cagnote * coef_gain).to_i
+        coef_gain = numeros_gagnants.to_f / 5
+        montant_gagne = (cagnote * coef_gain * coef_gain).to_i
         puts "Vous obtenez #{montant_gagne}€"
         cagnote -= montant_gagne
     elsif numeros_gagnants == 0
