3d823f1fb89d37a2aa41c407207c79eebaefc719
Dodo <donovan.persent@gmail.com>
2017-01-12 12:00:55 +0100

    Implement symbols in hash

--- Exos_Base/hash.rb
+++ Exos_Base/hash.rb
@@ -10,14 +10,13 @@ hash_namur = {
     "departement" => "Wallonie"
 }
 hash = {
-    "lomme" => hash_lomme,
-    "namur" => hash_namur
+    lomme: hash_lomme,
+    namur: hash_namur
 }
 
 
 print "Quel est le nom de votre ville? : "
-nom_ville =  gets.chomp
-nom_ville.downcase!
+nom_ville =  gets.chomp.downcase.to_sym
 
 if hash[nom_ville]
     puts "#{nom_ville.capitalize} a pour code postal #{hash[nom_ville]["code postal"]} et se trouve en #{hash[nom_ville]["pays"]} ( #{hash[nom_ville]["departement"]} ) "
