b734fadc60c1f70fd33fc67df1c665e05c8bb499
Dodo <donovan.persent@gmail.com>
2017-01-12 15:59:31 +0100

    French number until 99

--- Exos_Base/french_number.rb
+++ Exos_Base/french_number.rb
@@ -24,36 +24,40 @@ def num_in_french_v1 num
     end
 end
 
-jdilc
 def num_in_french_v2 num
     numbers_in_french_unit = ["zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"]
     numbers_in_french_deca = ["dix", "onze", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf"]
-    numbers_in_french_decamore = [numbers_in_french_unit, numbers_in_french_deca, "vingt", "trente", "quarante", "cinquant", "soixante", "soixante #{numbers_in_french_deca}", "quatre-vingt", "quatre-vingt #{numbers_in_french_deca}"]
+    numbers_in_french_decamore = [numbers_in_french_unit,"", "vingt ", "trente ", "quarante ", "cinquante ", "soixante ", "soixante ", "quatre-vingt ", "quatre-vingt "]
 
     if num < 99 || num > 0
         num_deca = num / 10
         num_unit = num % 10
-            if num_deca
-                outnumber = numbers_in_french_decamore
+        outnumber = ""
 
-
-
-
-
+        if num_deca
+            outnumber = numbers_in_french_decamore[num_deca]
+        end
+        if num_deca == 1 || num_deca == 7 || num_deca == 9
+            outnumber += numbers_in_french_deca[num_unit]
+        else
+            if num_unit == 1
+                outnumber += "et un"
+            else
+                outnumber += numbers_in_french_unit[num_unit]
+            end
+        end
+        return outnumber.capitalize
     else
         return "Entrée erronée"
     end
-
-    #return numbers_in_french_unit[num] if num < 9 || num > 0
-    #return  "Entrée erronée"
 end
 
 
 
 # ------ Application ------
 
-print "Entrez le nombre à traduire (v1) : "
-puts num_in_french_v1 gets.to_i
+#print "Entrez le nombre à traduire (v1) : "
+#puts num_in_french_v1 gets.to_i
 
 print "Entrez le nombre à traduire (v2) : "
 puts num_in_french_v2 gets.to_i
