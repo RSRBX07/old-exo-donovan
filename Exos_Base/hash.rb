d5bd52b6dceda05646c68cfa29b600a3a0b00d5e
Dodo <donovan.persent@gmail.com>
2017-01-12 11:44:07 +0100

    Hash modif: hash in hash

--- Exos_Base/hash.rb
+++ Exos_Base/hash.rb
@@ -1,16 +1,26 @@
-
-
-hash = {
-    "lomme" => 59160,
-    "namur" => 5000
+#exercice code postal
+hash_lomme = {
+    "code postal" => 59160,
+    "pays" => "France",
+    "departement" => "Nord"
 }
+hash_namur = {
+    "code postal" => 5000,
+    "pays" => "Belgique",
+    "departement" => "Wallonie"
+}
+hash = {
+    "lomme" => hash_lomme,
+    "namur" => hash_namur
+}
+
 
 print "Quel est le nom de votre ville? : "
 nom_ville =  gets.chomp
 nom_ville.downcase!
 
 if hash[nom_ville]
-    puts "#{nom_ville.capitalize} a pour code postal #{hash[nom_ville]}"
+    puts "#{nom_ville.capitalize} a pour code postal #{hash[nom_ville]["code postal"]} et se trouve en #{hash[nom_ville]["pays"]} ( #{hash[nom_ville]["departement"]} ) "
 else
     puts "#{nom_ville.capitalize} n'est pas référencée"
 end
