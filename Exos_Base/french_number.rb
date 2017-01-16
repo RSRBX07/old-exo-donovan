43e637a97c0c43f8f1ceda7fefc254a1f95d39f3
Dodo <donovan.persent@gmail.com>
2017-01-16 16:44:47 +0100

    refactor french number

--- Exos_Base/french_number.rb
+++ Exos_Base/french_number.rb
@@ -4,47 +4,44 @@
 
 # ------- Methods ------
 class FrenchNumber
-    def numbers_in_french_unit ; ["zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"] end
-    def numbers_in_french_deca ; ["dix", "onze", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf"] end
-    def numbers_in_french_decamore ; ["","", "vingt ", "trente ", "quarante ", "cinquante ", "soixante ", "soixante-", "quatre-vingt ", "quatre-vingt-"] end
+  def numbers_in_french_unit ; ["zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"] end
+  def numbers_in_french_deca ; ["dix", "onze", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf"] end
+  def numbers_in_french_decamore ; ["","", "vingt ", "trente ", "quarante ", "cinquante ", "soixante ", "soixante-", "quatre-vingt ", "quatre-vingt-"] end
 
-    def num_in_french_v2 num
+  def num_in_french_v2 num
+    return "Entrée erronée" unless input_valid?
+    return numbers_in_french_unit[num] if num < 10
 
-    if num < 99 || num > 0
-      num_deca = num / 10
-      num_unit = num % 10
-      
-      if num > 9
-        outnumber = ""
+    num_deca = num / 10
+    num_unit = num % 10
 
-        if num_deca
-          outnumber = numbers_in_french_decamore[num_deca]
-        end
-        if num_deca == 1 || num_deca == 7 || num_deca == 9
-          outnumber += numbers_in_french_deca[num_unit]
-        else
-          if num_unit == 1
-            outnumber += "et un"
-          else
-            if num_unit != 0
-                outnumber += numbers_in_french_unit[num_unit]
-            end
-          end
-        end
-      else
-        outnumber = numbers_in_french_unit[num]
-      end
+    outnumber = ""
 
-      return outnumber.capitalize
-    else
-      return "Entrée erronée"
+    if num_deca
+      outnumber = numbers_in_french_decamore[num_deca]
     end
+    if num_deca == 1 || num_deca == 7 || num_deca == 9
+      outnumber += numbers_in_french_deca[num_unit]
+    else
+      if num_unit == 1
+        outnumber += "et un"
+      else
+        if num_unit != 0
+            outnumber += numbers_in_french_unit[num_unit]
+        end
+      end
+    end
+
+
+    outnumber.capitalize
   end
 
+  private
 
-
+  def input_valid?
+    num < 99 || num > 0
+  end
     # ------ Application ------
-    print "Entrez le nombre à traduire (v2) : "
-    puts num_in_french_v2 gets.to_i
-
+    # print "Entrez le nombre à traduire (v2) : "
+    # puts num_in_french_v2 gets.to_i
 end
