15ee65e8a98557559a27b46a461314c67faa235e
Dodo <donovan.persent@gmail.com>
2017-01-19 14:48:08 +0100

    Counter Init Alpha

--- Vehicles/counter.rb
+++ Vehicles/counter.rb
@@ -1,16 +1,33 @@
 class Counter
+  attr_reader :count_value
   
-  
+  def initialize
+    @count_value = value || 0
+  end
+
   def add_one 
-    new_val = value + 1
+    new_val = @count_value + 1
     File.open "counter.txt", "w" do |counter_file|
       counter_file.write new_val
     end
   end
 
   def value
-    File.open "counter.txt", "r" do |counter_file|
-      counter_file.each-line {|line| return line.to_i}
+    if File.exist? "counter.txt" 
+      File.open "counter.txt", "r" do |counter_file|
+        counter_file.each_line do |line| 
+          line.to_i
+        end
+      end
+    else
+      File.open "counter.txt", "w" do |counter_file| 
+        counter_file.write(@count_value)
+        value
+      end 
     end
   end
 end
+
+counter = Counter.new
+
+puts counter.count_value 
