2a334b638bfa9837613c73f4ff1ced0ddddaf314
Dodo <donovan.persent@gmail.com>
2017-01-19 14:56:44 +0100

    counter simplifié

--- Vehicles/counter.rb
+++ Vehicles/counter.rb
@@ -3,31 +3,23 @@ class Counter
   
   def initialize
     @count_value = value || 0
+    add_one
   end
 
   def add_one 
-    new_val = @count_value + 1
+    new_val = value + 1
     File.open "counter.txt", "w" do |counter_file|
       counter_file.write new_val
     end
   end
 
   def value
-    if File.exist? "counter.txt" 
-      File.open "counter.txt", "r" do |counter_file|
-        counter_file.each_line do |line| 
-          line.to_i
-        end
-      end
-    else
-      File.open "counter.txt", "w" do |counter_file| 
-        counter_file.write(@count_value)
-        value
-      end 
+    File.open "counter.txt", "r" do |counter_file|
+      counter_file.each_line {|line| return line.to_i}
     end
   end
 end
 
-counter = Counter.new
 
+counter = Counter.new
 puts counter.count_value 
