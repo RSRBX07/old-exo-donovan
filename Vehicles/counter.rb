9094175b8c70850067fe4e800846426cc3367199
Dodo <donovan.persent@gmail.com>
2017-01-19 16:05:44 +0100

    Add check if File exist and create a new one if not

--- Vehicles/counter.rb
+++ Vehicles/counter.rb
@@ -1,25 +1,42 @@
 class Counter
   attr_reader :count_value
+  attr_reader :filename
   
   def initialize
+    @@filename ||= "counter.txt"
     @count_value = value || 0
     add_one
   end
 
+  def path
+    dirname = File.join(File.dirname(__FILE__), '/tmp/')
+    return (dirname + @@filename).to_s
+  end
+
   def add_one 
-    new_val = value + 1
-    File.open "counter.txt", "w" do |counter_file|
+    new_val = @count_value + 1
+    File.open path do |counter_file|
       counter_file.write new_val
     end
   end
 
   def value
-    File.open "counter.txt", "r" do |counter_file|
+    if !File.exist? path
+    File.new("./tmp/") 
+    File.open(path,"w") {|newFile| newFile.puts "0"} 
+    end
+    File.open path do |counter_file|
       counter_file.each_line {|line| return line.to_i}
     end
+  end
+
+  def delete_file
+    File.delete path
+    puts "le fichier #{@@filename} a été supprimé"
   end
 end
 
 
 counter = Counter.new
-puts counter.count_value 
+puts counter.count_value
+counter.delete_file Counter.filename if rand(0..100).to_i < 10
